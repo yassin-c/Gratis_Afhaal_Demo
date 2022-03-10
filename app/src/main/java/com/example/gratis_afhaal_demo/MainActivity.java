236009d09af8e4f3cb528a29b01d16f2b5fc059c
Yassin Chamlal <81307380+yassin-c@users.noreply.github.com>
2022-03-10 14:13:28 +0100

    Main aangepast en xml aangepast

--- app/src/main/java/com/example/gratis_afhaal_demo/MainActivity.java
+++ app/src/main/java/com/example/gratis_afhaal_demo/MainActivity.java
@@ -1,14 +1,85 @@
 package com.example.gratis_afhaal_demo;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    String naam;
+    String plaats;
+    String omschrijving;
+    ListView listView;
+    DatabaseReference databaseReference;
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance("https://fireloop-d0c40-default-rtdb.europe-west1.firebasedatabase.app/");
+        databaseReference = database.getReference("afhalen");
+        listView = (ListView)findViewById(R.id.listView1);
+        arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+        berichten();
+
+
     }
+
+
+    public void berichten() {
+
+        ValueEventListener chatListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                arrayList.clear();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String m = ds.getValue().toString();
+                    arrayList.add(m);
+
+                    //   Log.d("TAG", userId + " / " +  score);
+                }
+
+                // Collections.reverse(listChat);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+
+        };
+        databaseReference.addValueEventListener(chatListener);
+
+
+    }
+
+
+
+
+
 }
+

a3181fad057c026fa2fd6c817b7367edde0c229c
Yassin Chamlal <81307380+yassin-c@users.noreply.github.com>
2022-03-10 14:12:36 +0100

    Artikel class toegevoegd

---
+++ app/src/main/java/com/example/gratis_afhaal_demo/Artikel.java
@@ -0,0 +1,40 @@
+package com.example.gratis_afhaal_demo;
+
+public class Artikel {
+    private String naam;
+    private String plaats;
+    private String omschrijving;
+
+    public Artikel(){
+
+    }
+
+    public String getNaam(){
+        return  this.naam;
+    }
+
+    public void setNaam(String Naam){
+        this.naam = Naam;
+    }
+
+    public String getPlaats(){
+        return this.naam;
+    }
+
+    public void setPlaats(String Plaats){
+        this.plaats = Plaats;
+    }
+
+    public String getOmschrijving(){
+        return this.omschrijving;
+    }
+
+    public void setOmschrijving(String Omschrijving){
+        this.omschrijving = Omschrijving;
+    }
+
+
+
+
+
+}
